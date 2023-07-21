561cd7f57e2c0f3ae558514a0dc02dfab6aa81ec
KZ-DE <nebulacom005@gmail.com>
2023-07-21 18:36:33 +0700

    windows

--- coba/test.py
+++ coba/test.py
@@ -1,16 +1,3 @@
-import control
-import matplotlib.pyplot as plt
-
-sistem = control.TransferFunction([1, 1], [1, 3, 2])
-Kp, Ki, Kd = control.pidtune(sistem, 'ziegler-nichols')
-PID = control.TransferFunction([Kd, Kp, Ki], [1, 0])
-sistem_pid = control.feedback(sistem*PID, 1)
-t, y = control.step_response(sistem_pid)
-
-plt.plot(t, y)
-plt.xlabel('Waktu (s)')
-plt.ylabel('Output')
-plt.title('Respon Sistem dengan PID')
-plt.grid(True)
-plt.show()
-
+data = [1,3,2]
+for a,b in enumerate(data):
+    print(a,b)
--- dasar/8. list/operasi list.py
+++ dasar/8. list/operasi list.py
@@ -2,27 +2,31 @@
 BaseException
 
 data_angka = [1,4,6,3,7,9,4,7,0,4,7,3,0,3,4,6,3,5,2,7,9,2,3,4,5,7,3,6,3]
-print(f"banyak data = {data_angka}")
+print(f"banyak data = {data_angka}\n")
+
+# mengetahui jumlah idex dalam list
+lenght = len(data_angka)
+print(f"{lenght}\n")
 
 # menghitung banyaknya item dalam list
 data4 = data_angka.count(4)
 data9 = data_angka.count(9)
-print(f"banyaknya angka 4 = {data4}")
-print(f"banyaknya angka 9 = {data9}")
+print(f"banyaknya angka 4 = {data4}\n")
+print(f"banyaknya angka 9 = {data9}\n")
 
 # mengetahu posisi data(index)
 data = ["sudrun","bejo","slamet","ahri"]
-print(f"data nama = {data}")
+print(f"data nama = {data}\n")
 index_bejo = data.index("bejo")
-print(f"index bejo = {index_bejo}")
+print(f"index bejo = {index_bejo}\n")
 
 # mengurutkan data
 print(f"data angka = {data_angka}")
 data_angka.sort()
-print(f"data angka terurut = {data_angka}") # di urutukan terhadap angka terkecil terlebih dahulu
+print(f"data angka terurut = {data_angka}\n") # di urutukan terhadap angka terkecil terlebih dahulu
 print(data)
 data.sort()
-print(f"data terurut = {data}") # akan menampilkan sesuai aljabar huruf depannya
+print(f"data terurut = {data}\n") # akan menampilkan sesuai aljabar huruf depannya
 
 # membalik urutan data list
 data_angka.reverse()
