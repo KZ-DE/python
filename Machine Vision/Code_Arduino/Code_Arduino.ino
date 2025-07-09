0e62bcfdc0bfb20998d396f7fe92260ce5253a87
KZ-DE <nebulacom005@gmail.com>
2025-07-10 02:32:43 +0700

    Semester 7

--- Machine Vision/Code_Arduino/Code_Arduino.ino
+++ Machine Vision/Code_Arduino/Code_Arduino.ino
@@ -1,28 +1,14 @@
+int led = 2;
+int button = 7;
 void setup() {
-  Serial.begin(115200);
-  pinMode(PC13, OUTPUT);
-  analogWriteFrequency(50);  
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    int center_x = Serial.parseInt();
-    Serial.read();                     
-    int center_y = Serial.parseInt();  
-    digitalWrite(PC13, LOW);
-    int hasil = map(center_x, 190, 500, 0, 180);
-    setServoAngle(A0, constrain(hasil, 0, 180));
+  if (digitalRead(button) == HIGH) {
+    digitalWrite(led, HIGH);
   } else {
-    digitalWrite(PC13, HIGH);
-    setServoAngle(A0, 0 );
+    digitalWrite(led, LOW);
   }
-}
-
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void setServoAngle(int pin, int angle) {
-  int dutyCycle = mapFloat(angle, 0, 180, 5, 30);
-  analogWrite(pin, dutyCycle);
 }
